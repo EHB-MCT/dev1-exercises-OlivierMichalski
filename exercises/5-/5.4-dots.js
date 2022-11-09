065b8997948a562f54426d19afbf20b2da0d7537
Olivier Michalski <olivier.michalski@student.ehb.be>
2022-11-09 12:14:53 +0100

    1,2,3,4 en 6

---
+++ exercises/5-/5.4-dots.js
@@ -0,0 +1,27 @@
+"use strict";
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+drawDots();
+
+function drawDots() {
+
+    for (let i = 0; i < 50000; i++) {
+        let x = Math.random() * width;
+        let y = Math.random() * height;
+        if (x > width/1.5) {
+            context.fillStyle = "white";
+        }
+        else if (y > height/ 1.5){
+            context.fillStyle = "white";
+        }
+        else {
+            context.fillStyle = "green";
+        }
+        Utils.fillCircle(x, y, 5);
+    }
+
+}
